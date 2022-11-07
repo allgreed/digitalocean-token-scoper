7ba8a982ca404bd40c8e8383692f3d479cfa9b5f
Olgierd Kasprowicz <olgierd.kasprowicz@dynatrace.com>
2022-11-07 16:15:51 +0100

    Bump

--- default.nix
+++ default.nix
@@ -1,6 +1,5 @@
 let
   nixpkgs = builtins.fetchGit {
-    name = "nixos-unstable-2021-03-17";
     url = "https://github.com/nixos/nixpkgs/";
     ref = "refs/heads/nixos-unstable";
     rev = "266dc8c3d052f549826ba246d06787a219533b8f";
